16577ddc5e154b937d5107743e61c712f8158b6a
Alex Clarke <alex.j.tusa@gmail.com>
2025-10-10 13:38:47 -0600

    feat: Created the coder and sql agents

--- assets/agents/coder/tools.sh
+++ assets/agents/coder/tools.sh
@@ -3,7 +3,11 @@ set -e
 
 # @env LLM_OUTPUT=/dev/stdout The output path
 
-# @cmd Create a new file at the specified path with contents.
+PROMPT_UTILS="${LLM_ROOT_DIR:-$(dirname "${BASH_SOURCE[0]}")/..}/utils/prompt-utils.sh"
+# shellcheck disable=SC1090
+source "$PROMPT_UTILS"
+
+# @cmd Create a new file at the specified path with the given contents.
 # @option --path! The path where the file should be created
 # @option --contents! The contents of the file
 # shellcheck disable=SC2154
