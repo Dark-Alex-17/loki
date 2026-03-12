9b4c800597585ea11dffb33a23632a0a574d79cc
Alex Clarke <alex.j.tusa@gmail.com>
2026-03-12 09:08:17 -0600

    fix: The REPL .authenticate command works from within sessions, agents, and roles with pre-configured models

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -428,7 +428,8 @@ pub async fn run_repl_command(
                 None => println!("Usage: .model <name>"),
             },
             ".authenticate" => {
-                let client = init_client(config, None)?;
+                let current_model = config.read().current_model().clone();
+                let client = init_client(config, Some(current_model))?;
                 if !client.supports_oauth() {
                     bail!(
                         "Client '{}' doesn't either support OAuth or isn't configured to use it (i.e. uses an API key instead)",
